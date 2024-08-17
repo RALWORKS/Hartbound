b0f1deeb0b5a21207c188eda3af8fb51af0438dd
Rita Lester <r.a.lester121@gmail.com>
2024-08-16 18:34:42 -0600

    hide sidebar left during cutscenes

--- ui/main_screen.gd
+++ ui/main_screen.gd
@@ -1,1 +1,15 @@
 extends SubViewportContainer
+
+var game: Node
+
+func _process(_delta):
+	if get_parent() == null:
+		return
+	
+	if get_parent().chapter == null:
+		return
+	
+	if get_parent().chapter.cutscene == null:
+		$Menu2/ColorRect.visible = true
+	else:
+		$Menu2/ColorRect.visible = false
