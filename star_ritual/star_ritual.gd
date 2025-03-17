28aeac31ecf789cfd9b99f3bcdafa854e8e8f1af
Rita Lester <r.a.lester121@gmail.com>
2025-03-16 18:52:28 -0600

    star ritual: black out background

--- star_ritual/star_ritual.gd
+++ star_ritual/star_ritual.gd
@@ -6,6 +6,8 @@ var answers = []
 
 var sequence: Array[Star] = []
 
+var veil: ColorRect
+
 var LINE_WIDTH: int = 10
 var LINE_COLOR: Color = Color("white")
 var sigil_line: Line2D = Line2D.new()
@@ -30,6 +32,8 @@ func _ready():
 	sigil.make_stars(self)
 	add_loose_stars()
 	setup_answers()
+	make_veil()
+	veil.visible = false
 
 func style_line(l: Line2D):
 	add_child(l)
@@ -38,6 +42,7 @@ func style_line(l: Line2D):
 	l.joint_mode = Line2D.LINE_JOINT_ROUND
 	l.end_cap_mode = Line2D.LINE_CAP_ROUND
 	l.begin_cap_mode = Line2D.LINE_CAP_ROUND
+	l.z_index = 80
 	
 func setup_collision():
 	$Area/Hitbox.polygon = polygon.duplicate()
@@ -107,9 +112,43 @@ func get_player_position():
 	var p = game.player.position
 	return p - position
 
+func make_veil():
+	veil = ColorRect.new()
+	veil.color = Color("black")
+	veil.set_size(Vector2(3000, 3000))
+	veil.position = Vector2(0, 0)
+	veil.z_index = 100
+	var outline = Line2D.new()
+	for p in polygon + PackedVector2Array([polygon[0]]):
+		outline.add_point(p)
+	
+	outline.width = 5
+	outline.default_color = "#ff00ff"
+	outline.joint_mode = Line2D.LINE_JOINT_ROUND
+	outline.end_cap_mode = Line2D.LINE_CAP_ROUND
+	outline.begin_cap_mode = Line2D.LINE_CAP_ROUND
+	outline.z_index = 75
+	veil.add_child(outline)
+	
+	
+
 func start():
+	if not veil.get_parent():
+		get_parent().add_child(veil)
 	seconds_elapsed = 0.0
 	active = true
+	veil.visible = true
+	game.player.z_index = 200
+	z_index = 150
+	call_higher("daylight_off")
+
+func call_higher(meth):
+	var p = self
+	while (p and meth not in p):
+		p = p.get_parent()
+	if not p:
+		return
+	p.call(meth)
 
 func check_win():
 	if sequence in answers:
@@ -124,5 +163,9 @@ func die():
 	game.respawn_player()
 
 func stop():
+	veil.visible = false
 	reset()
 	active = false
+	game.player.z_index = 0
+	z_index = 0
+	call_higher("daylight_default")
