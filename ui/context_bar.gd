809d67e640bda17e1b14a19cfd7b49d2a2eda8c1
Rita Lester <r.a.lester121@gmail.com>
2025-05-24 17:35:56 -0600

    fix: hide ctx notifs. over cutscenes, modals

--- ui/context_bar.gd
+++ ui/context_bar.gd
@@ -3,12 +3,31 @@ extends SmartLabel
 var count = 0
 var old_count = 0
 
+var is_visible = true
+var was_visible = true
+
 func refresh_context():
 	count = game.context_notification_count
 	if count == old_count:
 		return
 	old_count = count
 	notify()
+
+func _get_visible():
+	if not game.chapter:
+		return false
+	if game.chapter.cutscene:
+		return false
+	if game.cur_modal:
+		return false
+	return true
+
+func refresh_visible():
+	is_visible = _get_visible()
+	if is_visible == was_visible:
+		return
+	visible = is_visible
+	was_visible = is_visible
 
 func notify():
 	$Player.stop()
@@ -19,3 +38,4 @@ func notify():
 
 func _process(_delta):
 	refresh_context()
+	refresh_visible()
