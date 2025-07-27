8843421a0c69f757020f5cd7098170f3d12dfe14
Rita Lester <r.a.lester121@gmail.com>
2025-07-26 21:34:05 -0600

    travel section seguq

--- travel/travel_events.gd
+++ travel/travel_events.gd
@@ -2,6 +2,7 @@ class_name TravelEvents
 extends Node
 
 var EventMap = {}
+@onready var segues: AnimationPlayer = $"../EventSegue"
 
 func get_next_event_by_id(id: String):
 	setup_event_map()
@@ -34,22 +35,27 @@ func setup_event_map():
 		EventMap[c.id] = c
 
 
-func play_turn(event):
+func play_turn(event, section):
 	$"..".iterate_turn()
-	print(event)
+	if segues.has_animation(section):
+		return play_event_after_segue(event, section)
 	event.play()
 
+
+func play_event_after_segue(event, section):
+	segues.connect("animation_finished", func (anim_name): event.play())
+	segues.play(section)
 
 func start_talking():
 	var big_talk = get_next_event_by_id("talk")
 	if big_talk:
-		return play_turn(big_talk)
+		return play_turn(big_talk, "talk")
 
 
 func start_thinking():
 	var big_think = get_next_event_by_id("think")
 	if big_think:
-		return play_turn(big_think)
+		return play_turn(big_think, "think")
 
 
 # Called when the node enters the scene tree for the first time.
