205a2852a31ebbcb4d465815533aec81b52390b4
RALWORKS <r.a.lester121@gmail.com>
2026-07-11 10:55:13 -0600

    feat: player base can walk

---
+++ anims/the_hartbound/animation_controller.gd
@@ -0,0 +1,76 @@
+@tool
+extends Node2D
+
+@export var frame_spacing: float = 0.3
+@export var animations_refreshing = true
+var _refresh_started = false
+
+@export var animation = "down_stopped"
+var last_animation = "down_stopped"
+
+func _animation_from_frames(frames_sequence):
+	var animation = Animation.new()
+	animation.length = frame_spacing * frames_sequence.size()
+	animation.loop_mode = Animation.LOOP_LINEAR
+	var track_index = animation.add_track(Animation.TYPE_VALUE)
+	animation.track_set_path(track_index, "Character:frame")
+	animation.track_set_interpolation_type(track_index, Animation.INTERPOLATION_NEAREST)
+	
+	var trace = 0
+	
+	for cur_frame in frames_sequence:
+		animation.track_insert_key(track_index, trace, cur_frame)
+		trace += frame_spacing
+
+	return animation
+
+func _make_walk_animations():
+	_refresh_started = true
+	var library = AnimationLibrary.new()
+	
+	for key in ANIMATIONS.keys():
+		var value = ANIMATIONS[key]
+		library.add_animation(key, _animation_from_frames(value))
+
+	$AnimationPlayer.remove_animation_library("movement")
+	$AnimationPlayer.add_animation_library("movement", library)
+	animations_refreshing = false
+	_refresh_started = false
+
+var ANIMATIONS = {
+	"down": [28, 29, 30, 31, 32, 33, 34, 35],
+	"down_stopped": [27],
+	"up": [19, 20, 21, 22, 23, 24, 25, 26],
+	"up_stopped": [18],
+	"left": [1, 2, 3, 4, 5, 6, 7, 8],
+	"left_stopped": [0],
+	"down_left": [37, 38, 39, 40, 41, 42, 43, 44],
+	"down_left_stopped": [36],
+	"up_left": [10, 11, 12, 13, 14, 15, 16, 17],
+	"up_left_stopped": [9],
+	"ride_up": [47],
+	"ride_down": [49],
+	"ride_left": [45],
+	"ride_up_left": [46],
+	"ride_down_left": [48],
+	"mount_f1": [50],
+	"mount_f2": [51],
+}
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	refresh_animation()
+
+func refresh_animation():
+	last_animation = animation
+	if not $AnimationPlayer.has_animation("movement/" + animation):
+		return
+	$AnimationPlayer.play("movement/" + animation)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if animations_refreshing and not _refresh_started:
+		_make_walk_animations()
+	if animation != last_animation:
+		refresh_animation()
